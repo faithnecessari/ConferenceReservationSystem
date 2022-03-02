ac344f4c1135cbf07e72c37566fef76429e4e1fd
faithnecessari <necesariofaith@gmail.com>
2022-03-02 22:07:34 +0800

    Update forms.py

--- ConferenceReservationSystem/JulaneHotel/forms.py
+++ ConferenceReservationSystem/JulaneHotel/forms.py
@@ -6,3 +6,10 @@ class RoomsForm(forms.ModelForm):
     class Meta:
             model = Rooms
             fields= "__all__"
+
+
+class CustomerForm(forms.ModelForm):
+
+    class Meta:
+            model = Customer
+            fields= "__all__"
