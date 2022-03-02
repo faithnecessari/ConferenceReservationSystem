bc2136b2a8e869ce3a93c0e231ba5bff8d38be66
faithnecessari <necesariofaith@gmail.com>
2022-03-02 18:55:06 +0800

    Update urls.py

--- ConferenceReservationSystem/ConferenceReservationSystem/urls.py
+++ ConferenceReservationSystem/ConferenceReservationSystem/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('about', views.MyAboutView.as_view(),name="my_about_view"),
     path('LogIn', views.MyLogInView.as_view(),name="my_LogIn_view"),
     path('adminLogIn', views.MyadminLogInView.as_view(),name="my_adminLogIn_view"),
+    path('adminDashboard', views.MyadminDashboardView.as_view(),name="my_adminDashboard_view"),
 ]
