54ae6614116323b8e5d930e9503fbe6e910456e4
faithnecessari <necesariofaith@gmail.com>
2022-03-15 15:36:53 +0800

    Update models.py

--- ConferenceReservationSystem/JulaneHotel/models.py
+++ ConferenceReservationSystem/JulaneHotel/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Rooms(models.Model):
 	roomtype = models.CharField(max_length=20)
+	dateofuse = models.Datefield()
 	timeslot = models.CharField(max_length=20)
 	price = models.IntegerField()
 
